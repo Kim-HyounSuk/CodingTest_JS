b377e30f21b954d96d9f0635c8b61fc25e42aec8
Vanlan <82691577+Kim-HyounSuk@users.noreply.github.com>
2023-11-27 20:21:32 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 21.32 ms, Memory: 83.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.js
@@ -0,0 +1,10 @@
+function solution(arr)
+{
+    let answer = [];
+    
+    arr.forEach((item) => {
+        if(answer[answer.length - 1] !== item) answer.push(item);
+    });
+    
+    return answer;
+}
