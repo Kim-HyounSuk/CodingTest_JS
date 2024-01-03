ca60060180b2506525271bca027ba0fed5d00d26
Vanlan <82691577+Kim-HyounSuk@users.noreply.github.com>
2024-01-03 21:42:51 +0900

    [level 2] Title: 카펫, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/42842. 카펫/카펫.js
@@ -0,0 +1,16 @@
+function solution(brown, yellow) {
+    let answer = [];
+    let squareSize = brown + yellow;
+    
+    // height는 width 보다 짧거나 같아야 한다 (height의 최소값는 3 부터 시작)
+    // (x - 2) * (y - 2) = yellow
+    for(let height = 3; height <= (brown + yellow) / height; height++) {
+        let width = Math.floor((brown + yellow) / height);
+        if((width - 2) * (height - 2) === yellow) {
+            answer = [width, height];
+            break;
+        }
+    }
+    
+    return answer;
+}
