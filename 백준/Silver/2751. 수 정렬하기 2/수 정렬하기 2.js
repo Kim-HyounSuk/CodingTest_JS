cd325f065be430fdd63ceeff8a75017bdc59814f
Vanlan <82691577+Kim-HyounSuk@users.noreply.github.com>
2024-04-09 14:33:11 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 2212 ms, Memory: 173988 KB -BaekjoonHub

---
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.js
@@ -0,0 +1,12 @@
+const fs = require('fs');
+const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
+const input = fs.readFileSync(filePath).toString().trim().split('\n');
+
+const solution = () => {
+  const N = input.shift();
+
+  input.sort((a, b) => a - b);
+  console.log(input.join('\n'));
+};
+
+solution();
