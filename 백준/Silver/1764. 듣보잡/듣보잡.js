45e7c24340112d9e77de3c4265513fd8952b2c31
Vanlan <82691577+Kim-HyounSuk@users.noreply.github.com>
2024-05-03 02:27:56 +0900

    [Silver IV] Title: 듣보잡, Time: 312 ms, Memory: 48432 KB -BaekjoonHub

---
+++ 백준/Silver/1764. 듣보잡/듣보잡.js
@@ -0,0 +1,27 @@
+const fs = require('fs');
+const filePath = process.platform === 'linux' ? '/dev/stdin' : './input.txt';
+const input = fs
+  .readFileSync(filePath)
+  .toString()
+  .trim()
+  .split('\n')
+  .map((value) => value.replace(/\r/, ''));
+
+const solution = () => {
+  const [N, M] = input.shift().split(' ').map(Number);
+  const names = {};
+  const answer = [0, []];
+
+  input.forEach((name) => {
+    if (!names[name]) names[name] = 1;
+    else {
+      answer[0]++;
+      answer[1].push(name);
+    }
+  });
+
+  console.log(answer[0]);
+  console.log(answer[1].sort().join('\n'));
+};
+
+solution();
