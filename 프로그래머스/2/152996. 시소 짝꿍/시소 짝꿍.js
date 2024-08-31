844b9a2c9e8290cbeb819e595236f3da08ef4ef6
Vanlan <82691577+Kim-HyounSuk@users.noreply.github.com>
2024-09-01 07:16:20 +0900

    [level 2] Title: 시소 짝꿍, Time: 53.03 ms, Memory: 42.6 MB -BaekjoonHub

--- 프로그래머스/2/152996. 시소 짝꿍/시소 짝꿍.js
+++ 프로그래머스/2/152996. 시소 짝꿍/시소 짝꿍.js
@@ -1,20 +1,19 @@
 function solution(weights) {
-    var answer = 0;
-    const ratios = [1, 2/3, 2/4, 3/4];
-    const checked = {};
-    
-    weights.sort((a, b) => a - b).forEach((weight) => {
-        ratios.forEach((ratio) => {
-            const equal = weight * ratio;
-            
-            if(checked[equal]) {
-                answer += checked[equal];
-            }
-        });
+    let answer = 0;
+    const countMap = {};
+
+    weights.sort((a, b) => a - b);
+    // weights 배열을 순회하며 가능한 짝을 찾는다
+    for (let weight of weights) {
+        // 각각의 비율을 고려
+        answer += (countMap[weight] || 0);           // 1:1 짝 찾기
+        answer += (countMap[weight * 2 / 3] || 0);   // 2:3 짝 찾기
+        answer += (countMap[weight * 3 / 4] || 0);   // 3:4 짝 찾기
+        answer += (countMap[weight * 2 / 4] || 0);   // 2:4 짝 찾기
         
-        if(checked[weight]) checked[weight] += 1;
-        else checked[weight] = 1;
-    });
+        // 현재 weight 값을 카운트 맵에 추가
+        countMap[weight] = (countMap[weight] || 0) + 1;
+    }
     
     return answer;
 }
