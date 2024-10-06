9f59eaa4acffb0c7c617552b0a04bad42444eabb
Vanlan <82691577+Kim-HyounSuk@users.noreply.github.com>
2024-10-06 10:19:06 +0900

    [level 2] Title: 숫자의 표현, Time: 0.32 ms, Memory: 33.3 MB -BaekjoonHub

--- 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.js
+++ 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.js
@@ -1,15 +1,28 @@
-function solution(n) {
-    let answer = 0;
+// function solution(n) {
+//     let answer = 0;
     
-    for(let i = 0; i < n; i++) {
-        let sum = 0;
-        for(let j = i + 1; j <= n; j++) {
-            sum += j
+//     for(let i = 0; i < n; i++) {
+//         let sum = 0;
+//         for(let j = i + 1; j <= n; j++) {
+//             sum += j
             
-            if(sum === n) answer++;
-            if(sum > n) break;
-        }
-    }
+//             if(sum === n) answer += 1;
+//             if(sum > n) break;
+//         }
+//     }
     
-    return answer;
+//     return answer;
+// }
+
+function solution(n) {
+  let answer = 0;
+  
+  for(let i = 0; i <= n; i++) {
+  	if(n%i === 0 && i%2 === 1) answer++;
+  }
+  
+  return answer;
 }
+
+// "주어진 자연수를 연속된 자연수의 합으로 표현하는 방법의 수와
+// 주어진 수의 홀수인 약수 갯수는 같다."
